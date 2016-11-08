bb6ed161bc50aca4c68a83d9a2f25fab860b6968
wangtaihong <wangtaihong8@163.com>
2016-11-08 16:26:26 +0800

    Update 3.php
    更正了重复a标签嵌套的问题

--- 3.php
+++ 3.php
@@ -1,21 +1,89 @@
 <?php
+//为了解决a标签重复嵌套，先把数组按长度排序，长的在前面
+
 /**
- * 自字符串转换为a标签
- * @param  array $arr 字串数组
- * @param  string $str 要处理的字符串
- * @return array      结果
+ * 先对字符串数组按长度逆序排序
+ * @param  array $arr [description]
+ * @return array      [description]
  */
+function mySort($arr){
+	$len=count($arr);
+	for($i=0; $i < $len; $i++){
+		for($j = $i+1;$j < $len; $j++){
+			if ( (strlen($arr[$j]) > (strlen($arr[$i])))) {
+				$temp=$arr[$i];
+				$arr[$i]=$arr[$j];
+				$arr[$j]=$temp;
+			}
+		}
+	}
+	return $arr;
+}
+/*
+array(4) {
+  [0]=>
+  string(24) "United States of America"
+  [1]=>
+  string(17) "Chinese companies"
+  [2]=>
+  string(13) "United States"
+  [3]=>
+  string(7) "Chinese"
+}
+ */
+
+/*
+//这个函数有错误，于是重写了后面的函数
 function replace_to_Atarget($arr,$str){
 	foreach ($arr as $key => $value) {
 		$str =str_replace($value,"<a href='$value' >$value</a>", $str);
 	}
 	return $str;
 }
+ */
 
-$str=replace_to_Atarget($array,$text_string);
+
+/**
+ * 子字符串替换为a标签
+ * @param  array $arr 字串数组
+ * @param  string $str 要处理的字符串
+ * @return array      结果
+ */
+function replace_to_Atarget($arr,$str){
+	$flag=array();
+	//为了解决a标签重复嵌套，把test里的关键词替换成一个唯一id
+	//每一个唯一id对应一个替换后的a标签，存在数组flag中
+	foreach ($arr as $key => $value) {
+		//value替换为：
+		$to=str_replace($value, "<a href='$value' >$value</a>", $value);
+		//生成一个唯一id
+		$uniqid=uniqid();
+		//使用唯一id替换text中的关键字
+		$str=str_replace($value, $uniqid, $str);
+		//把唯一id和对应的a标签存在数组
+		$flag[$uniqid]=$to;
+	}
+
+	//最后循环flag数组，把text中的唯一id换成对应的a标签
+	foreach ($flag as $key => $value) {
+		$str=str_replace($key, $value, $str);
+	}
+	return $str;
+}
+
+
+$array = ['Chinese', 'Chinese companies', 'United States', 'United States of America'];
+$text_string = 'Chinese是中国人的意思，Chinese companies是什么意思? Chinese 与 Chinese companies 是什么关系？Chinese companies不能代替Chinese. United States是美国，United States of America也是美国，但United States也不能代替United States of America';
+
+//数组排序：
+echo "<pre>";
+$arr=mySort($array);
+var_dump($arr);
+
+//替换a标签：
+$str=replace_to_Atarget($arr,$text_string);
 var_dump($str);
+
 /*
-结果：
-string(524) "<a href='Chinese' >Chinese</a>是中国人的意思，<a href='Chinese' >Chinese</a> companies是什么意思? <a href='Chinese' >Chinese</a> 与 <a href='Chinese' >Chinese</a> companies 是什么关系？<a href='Chinese' >Chinese</a> companies不能代替<a href='Chinese' >Chinese</a>. <a href='United States' >United States</a>是美国，<a href='United States' >United States</a> of America也是美国，但<a href='United States' >United States</a>也不能代替<a href='United States' >United States</a> of America"
-<a href='Chinese' >Chinese</a>是中国人的意思，<a href='Chinese' >Chinese</a> companies是什么意思? <a href='Chinese' >Chinese</a> 与 <a href='Chinese' >Chinese</a> companies 是什么关系？<a href='Chinese' >Chinese</a> companies不能代替<a href='Chinese' >Chinese</a>. <a href='United States' >United States</a>是美国，<a href='United States' >United States</a> of America也是美国，但<a href='United States' >United States</a>也不能代替<a href='United States' >United States</a> of America
+正则匹配效率会更高，会省去很多麻烦，目前我正在学习正则匹配
  */
