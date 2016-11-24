f691a85b9fdbc90deb1c98087c301cb48c14f490
wangtaihong <wangtaihong8@163.com>
2016-11-25 03:27:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>citybrain</title>
+	<meta property="wb:webmaster" content="afc9825892c5a388" />
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>wellcome</h1>
+<hr>
+<?php echo phpinfo();?>
+</body>
+</html>
